6377554ad7d9b1d6d05a7d5dbf785cbf1cf32d02
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-03-05 15:31:20 +0100

    initialize mutex + philos

--- src/philo.c
+++ src/philo.c
@@ -21,6 +21,16 @@ int is_numeric(const char *s)
 
 int philo(t_info *info)
 {
-	t_philo mdr[info->nb_p];
+	t_philo philos[info->nb_p];
+	pthread_mutex_t mutexs[info->nb_p];
+
+	for (int i = 0 ; i < info->nb_p ; i++) {
+		pthread_mutex_init(&mutexs[i], NULL);
+		philos[i].left = &mutexs[i];
+		philos[i].right = &mutexs[(i == 0 ? info->nb_p : i ) - 1];
+		philos[i].nb_eat = 0;
+		philos[i].status = REST;
+	}
+
 	return (SUCCESS);
 }
