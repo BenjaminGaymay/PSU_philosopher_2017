be05c43c5af0a5dd3a2816b10e8ea70ca8cde6b2
Benoit Pingris <benoit.pingris@epitech.eu>
2018-03-06 16:55:45 +0100

    remove all printf

--- src/philo.c
+++ src/philo.c
@@ -25,11 +25,11 @@ size_t get_stick(t_philo *philo)
 	size_t nb = NONE;
 
 	if (pthread_mutex_trylock(philo->left) == 0) {
-		printf("%d GET LEFT\n", philo->id);
+		/* printf("%d GET LEFT\n", philo->id); */
 		nb += LEFT;
 	}
 	if (pthread_mutex_trylock(philo->right) == 0) {
-		printf("%d GET RIGHT\n", philo->id);
+		/* printf("%d GET RIGHT\n", philo->id); */
 		nb += RIGHT;
 	}
 	philo->nb_stick = nb;
@@ -41,68 +41,67 @@ void rest(t_philo *philo)
 	size_t nb_stick = get_stick(philo);
 
 	lphilo_sleep();
-	printf("REST %d : je dors\n", philo->id);
+	/* printf("REST %d : je dors\n", philo->id); */
 	if (nb_stick == LEFT || nb_stick == RIGHT) {
 		philo->status = THINK;
-		printf("REST %d : je vais THINK\n", philo->id);
+		/* printf("REST %d : je vais THINK\n", philo->id); */
 	}
 	else if (nb_stick == BOTH) {
 		philo->status = EAT;
-		printf("REST %d : je vais EAT\n", philo->id);
+		/* printf("REST %d : je vais EAT\n", philo->id); */
 	}
 }
 
 void think(t_philo *philo)
 {
 	if (philo->nb_stick == LEFT) {
-		printf("THINK %d : je prend LEFT\n", philo->id);
+		/* printf("THINK %d : je prend LEFT\n", philo->id); */
 		lphilo_take_chopstick(philo->left);
 		philo->l_stick = true;
 	}
 	else {
-		printf("THINK %d : je prend RIGHT\n", philo->id);
+		/* printf("THINK %d : je prend RIGHT\n", philo->id); */
 		lphilo_take_chopstick(philo->right);
 		philo->r_stick = true;
 	}
-	printf("THINK %d : je pense\n", philo->id);
+	/* printf("THINK %d : je pense\n", philo->id); */
 	lphilo_think();
 	usleep(500);
 	while (get_stick(philo) == NONE)
 	{
 		usleep(500);
-		printf("THINK %d : je pense (boucle)\n", philo->id);
-		lphilo_think();
+		/* printf("THINK %d : je pense (boucle)\n", philo->id); */
 	}
-	printf("THINK %d : je vais EAT\n", philo->id);
+	/* printf("THINK %d : je vais EAT\n", philo->id); */
 	philo->status = EAT;
 }
 
 void eat(t_philo *philo)
 {
 	if (philo->l_stick) {
-		printf("EAT %d : je prend RIGHT\n", philo->id);
+		/* printf("EAT %d : je prend RIGHT\n", philo->id); */
 		lphilo_take_chopstick(philo->right);
 	}
 	else if (philo->r_stick) {
-		printf("EAT %d : je prend LEFT\n", philo->id);
+		/* printf("EAT %d : je prend LEFT\n", philo->id); */
 		lphilo_take_chopstick(philo->left);
 	}
 	else {
-		printf("EAT %d : je prend RIGHT et LEFT\n", philo->id);
+		/* printf("EAT %d : je prend RIGHT et LEFT\n", philo->id); */
 		lphilo_take_chopstick(philo->left);
 		lphilo_take_chopstick(philo->right);
 	}
-	printf("EAT %d : je mange\n", philo->id);
+	/* printf("EAT %d : je mange\n", philo->id); */
 	lphilo_eat();
 	usleep(1000);
-	printf("EAT %d : je lache LEFT et RIGHT\n", philo->id);
+	/* printf("EAT %d : je lache LEFT et RIGHT\n", philo->id); */
 	lphilo_release_chopstick(philo->left);
 	pthread_mutex_unlock(philo->left);
 	lphilo_release_chopstick(philo->right);
 	pthread_mutex_unlock(philo->right);
 	philo->l_stick = false;
 	philo->r_stick = false;
-	printf("EAT : je vais dormir\n");
+	/* printf("EAT : je vais dormir\n"); */
 	philo->status = REST;
 	philo->nb_eat -= 1;
 }
